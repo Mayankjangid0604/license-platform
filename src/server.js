38e9bc14c2837c845470f37bae8893d92f9860fa
Mayankjangid0604 <jangidmayank768@gmail.com>
2026-01-09 00:48:08 +0530

    Enable admin and api routes

--- src/server.js
+++ src/server.js
@@ -1,18 +1,17 @@
 import express from "express";
-import validateRoutes from "./routes/validate.routes.js";
-import adminRoutes from "./routes/admin.routes.js";
 import healthRoutes from "./routes/health.routes.js";
+import adminRoutes from "./routes/admin.routes.js";
+import validateRoutes from "./routes/validate.routes.js";
 import "./db/database.js";
 
 const app = express();
 app.use(express.json());
 
-// order does NOT matter now
 app.use("/health", healthRoutes);
+app.use("/admin", adminRoutes);   // ✅ THIS WAS MISSING
 app.use("/api", validateRoutes);
-app.use("/admin", adminRoutes);
 
-app.get("/", (_, res) => {
+app.get("/", (req, res) => {
   res.send("Universal License Platform ONLINE");
 });
 
