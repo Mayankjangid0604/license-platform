62d2f88eaa547d264aab976eee6df9933291ab53
Mayankjangid0604 <jangidmayank768@gmail.com>
2026-01-09 00:38:02 +0530

    Fix health endpoint

--- src/routes/health.routes.js
+++ src/routes/health.routes.js
@@ -1,9 +1,10 @@
+// src/routes/health.routes.js
 import express from "express";
 import { SERVER_ID } from "../config/constants.js";
 
 const router = express.Router();
 
-router.get("/health", (_, res) => {
+router.get("/", (_, res) => {
   res.json({
     status: "ONLINE",
     server_id: SERVER_ID,
--- src/server.js
+++ src/server.js
@@ -1,21 +1,20 @@
 import express from "express";
 import validateRoutes from "./routes/validate.routes.js";
 import adminRoutes from "./routes/admin.routes.js";
-import "./db/database.js";
 import healthRoutes from "./routes/health.routes.js";
-
-
+import "./db/database.js";
 
 const app = express();
 app.use(express.json());
-app.use("/health", healthRoutes);
+
 app.use("/api", validateRoutes);
 app.use("/admin", adminRoutes);
+app.use("/health", healthRoutes);
 
 app.get("/", (_, res) => {
   res.send("Universal License Platform ONLINE");
 });
 
-app.listen(3000, () => {
-  console.log("License server running on port 3000");
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`License server running on port ${process.env.PORT || 3000}`);
 });
